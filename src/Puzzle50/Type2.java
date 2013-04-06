b121a74b18d1edd812fb9e12df1e93a1bdeede6f
apple <apple@applematoMacBook-Pro-2.local>
2013-04-06 17:21:32 +0800

    second commit

--- src/Puzzle50/Type2.java
+++ src/Puzzle50/Type2.java
@@ -2,6 +2,6 @@ package Puzzle50;
 
 public class Type2 {
 	public static void main(String[] args) {
-		System.out.print(new Type2() instanceof String);
+//		System.out.print(new Type2() instanceof String);
 	}
 }
